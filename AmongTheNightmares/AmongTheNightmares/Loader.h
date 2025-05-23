c662eaaacfc358d5c6273c16c9eb6fe9931edfe4
Evgenii Bazovoi <bazovoyz@mail.ru>
2025-05-23 18:14:38 +0300

    Update

--- AmongTheNightmares/AmongTheNightmares/Loader.h
+++ AmongTheNightmares/AmongTheNightmares/Loader.h
@@ -1,1 +1,10 @@
 #pragma once
+
+
+class Loader {
+public:
+	void LoadLocation();
+	void LoadText();
+	void LoadEnemy();
+	void LoadLoot();
+};
--- AmongTheNightmares/AmongTheNightmares/Room.h
+++ AmongTheNightmares/AmongTheNightmares/Room.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "Enemy.h"
 
 class Room {
+private:
+	std::string RoomText;
+	std::vector <Weapon> Loot_W;
+	std::vector <Enemy> enemies;
+public:
 
 };
